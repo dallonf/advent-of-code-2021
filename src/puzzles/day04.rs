5228044c5407883491142e02242a1dd17939327b
Dallon Feldner <dallon.feldner@gmail.com>
2021-12-04 08:13:25 -0600

    Day 4 Part One

--- src/puzzles/day04.rs
+++ src/puzzles/day04.rs
@@ -1,12 +1,13 @@
 // Day 4: Giant Squid
 use crate::prelude::*;
 
-// lazy_static! {
-//     static ref PUZZLE_INPUT: Box<[&'static str]> = include_lines!("day00_input.txt");
-// }
+lazy_static! {
+    static ref PUZZLE_INPUT: Game =
+        Game::parse_from_iter(&mut include_str!("day04_input.txt").lines()).unwrap();
+}
 
-pub fn part_one() -> String {
-    "Hello world".to_string()
+pub fn part_one() -> Option<u32> {
+    PUZZLE_INPUT.clone().get_winning_score()
 }
 
 const ROW_SIZE: usize = 5;
@@ -16,6 +17,7 @@ const GRID_SIZE: usize = ROW_SIZE * COLUMN_SIZE;
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 struct BoardTile(u8, bool);
 
+#[derive(Copy, Clone)]
 struct Board {
     grid: [BoardTile; GRID_SIZE],
 }
@@ -48,8 +50,53 @@ impl Board {
         }
         Ok(Board { grid })
     }
+
+    fn tile_at(&self, x: usize, y: usize) -> &BoardTile {
+        &self.grid[y * ROW_SIZE + x]
+    }
+
+    fn mark(&mut self, next_num: u8) {
+        for tile in self.grid.iter_mut() {
+            if tile.0 == next_num {
+                tile.1 = true
+            }
+        }
+    }
+
+    fn rows(&self) -> impl Iterator<Item = impl Iterator<Item = &BoardTile>> {
+        (0..COLUMN_SIZE).map(move |y| (0..ROW_SIZE).map(move |x| self.tile_at(x, y)))
+    }
+
+    fn columns(&self) -> impl Iterator<Item = impl Iterator<Item = &BoardTile>> {
+        (0..ROW_SIZE).map(move |x| (0..COLUMN_SIZE).map(move |y| self.tile_at(x, y)))
+    }
+
+    fn is_winning(&self) -> bool {
+        let mut lines = self
+            .rows()
+            .map(|row| row.collect::<Box<[&BoardTile]>>())
+            .chain(self.columns().map(|column| column.collect()));
+
+        lines.any(|line| line.into_iter().all(|BoardTile(_, marked)| *marked))
+    }
+
+    fn mark_and_get_score_if_winning(&mut self, next_num: u8) -> Option<u32> {
+        self.mark(next_num);
+        if self.is_winning() {
+            let unmarked: u32 = self
+                .grid
+                .iter()
+                .filter(|tile| !tile.1)
+                .map(|tile| tile.0 as u32)
+                .sum();
+            Some(unmarked * next_num as u32)
+        } else {
+            None
+        }
+    }
 }
 
+#[derive(Clone)]
 struct Game {
     sequence: Box<[u8]>,
     boards: Box<[Board]>,
@@ -83,11 +130,27 @@ impl Game {
             boards: boards.into(),
         })
     }
+
+    fn get_winning_score(&mut self) -> Option<u32> {
+        for &next_num in self.sequence.iter() {
+            for board in self.boards.iter_mut() {
+                if let Some(score) = board.mark_and_get_score_if_winning(next_num) {
+                    return Some(score);
+                }
+            }
+        }
+        None
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    lazy_static! {
+        static ref EXAMPLE_INPUT: Game =
+            Game::parse_from_iter(&mut include_str!("day04_example.txt").lines()).unwrap();
+    }
 
     #[test]
     fn parse() {
@@ -96,9 +159,58 @@ mod tests {
         assert_eq!(result.boards.len(), 3);
     }
 
-    // #[test]
-    // fn part_one_answer() {
-    //     let result = part_one();
-    //     assert_eq!(result, "Hello world! (3)");
-    // }
+    #[test]
+    fn board_does_not_win() {
+        let mut board = Board::parse_from_iter(
+            &mut [
+                "22 13 17 11  0",
+                " 8  2 23  4 24",
+                "21  9 14 16  7",
+                " 6 10  3 18  5",
+                " 1 12 20 15 19",
+            ]
+            .into_iter(),
+        )
+        .unwrap();
+
+        for num in [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24] {
+            board.mark(num);
+        }
+
+        assert_eq!(board.is_winning(), false);
+    }
+
+    #[test]
+    fn board_wins() {
+        let mut board = Board::parse_from_iter(
+            &mut [
+                "14 21 17 24  4",
+                "10 16 15  9 19",
+                "18  8 23 26 20",
+                "22 11 13  6  5",
+                " 2  0 12  3  7",
+            ]
+            .into_iter(),
+        )
+        .unwrap();
+
+        for num in [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24] {
+            board.mark(num);
+        }
+
+        assert_eq!(board.is_winning(), true);
+    }
+
+    #[test]
+    fn winning_score() {
+        let mut game = EXAMPLE_INPUT.clone();
+        let result = game.get_winning_score();
+        assert_eq!(result, Some(4512));
+    }
+
+    #[test]
+    fn part_one_answer() {
+        let result = part_one();
+        assert_eq!(result, Some(5685));
+    }
 }
