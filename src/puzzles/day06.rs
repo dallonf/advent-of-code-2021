098a21147f30907e3aeafbc49f2e29dcaee7e297
Dallon Feldner <dallon.feldner@gmail.com>
2021-12-06 07:21:49 -0600

    Day 6 Part Two

--- src/puzzles/day06.rs
+++ src/puzzles/day06.rs
@@ -3,6 +3,7 @@ use crate::prelude::*;
 
 const CYCLE_LENGTH: u8 = 7;
 const MATURITY: u8 = 2;
+const TOTAL_LENGTH: usize = (CYCLE_LENGTH + MATURITY) as usize;
 
 lazy_static! {
     static ref PUZZLE_INPUT: Box<[u8]> = include_str!("day06_input.txt")
@@ -11,28 +12,58 @@ lazy_static! {
         .collect();
 }
 
-pub fn part_one() -> usize {
-    let mut all_fish = PUZZLE_INPUT.clone().into_vec();
-    count_fish_after_days(&mut all_fish, 80)
+pub fn part_one() -> Result<usize, String> {
+    let mut all_fish = FishSimulation::from_fish_list(&PUZZLE_INPUT)?;
+    Ok(all_fish.count_fish_after_days(80))
 }
 
-fn tick(all_fish: &mut Vec<u8>) {
-    let count_at_zero = all_fish.iter().filter(|&&x| x == 0).count();
-    for fish in all_fish.iter_mut() {
-        *fish = match *fish {
-            0 => CYCLE_LENGTH - 1,
-            life => life - 1,
-        };
-    }
-    let mut new_fish = vec![CYCLE_LENGTH + MATURITY - 1; count_at_zero];
-    all_fish.append(&mut new_fish);
+pub fn part_two() -> Result<usize, String> {
+    let mut all_fish = FishSimulation::from_fish_list(&PUZZLE_INPUT)?;
+    Ok(all_fish.count_fish_after_days(256))
 }
 
-fn count_fish_after_days(all_fish: &mut Vec<u8>, days: usize) -> usize {
-    for _ in 0..days {
-        tick(all_fish);
+struct FishSimulation {
+    fish_by_days_until_spawn: [usize; TOTAL_LENGTH],
+}
+
+impl FishSimulation {
+    fn from_fish_list(list: &[u8]) -> Result<Self, String> {
+        let mut fish_by_days_until_spawn = [0; TOTAL_LENGTH];
+        for &fish_state in list {
+            if fish_state >= TOTAL_LENGTH as u8 {
+                return Err(format!(
+                    "Maximum value is {}, but received a {}",
+                    TOTAL_LENGTH, fish_state
+                ));
+            }
+            fish_by_days_until_spawn[fish_state as usize] += 1;
+        }
+        Ok(FishSimulation {
+            fish_by_days_until_spawn,
+        })
     }
-    all_fish.len()
+
+    fn tick(&mut self) {
+        let fish_to_spawn = self.fish_by_days_until_spawn[0];
+        for i in 1..(TOTAL_LENGTH) {
+            self.fish_by_days_until_spawn[i - 1] = self.fish_by_days_until_spawn[i];
+        }
+        // reset cycle
+        self.fish_by_days_until_spawn[CYCLE_LENGTH as usize - 1] += fish_to_spawn;
+        // add new fish
+        self.fish_by_days_until_spawn[TOTAL_LENGTH - 1] = fish_to_spawn;
+    }
+
+    fn count_fish(&self) -> usize {
+        self.fish_by_days_until_spawn.iter().sum()
+    }
+
+    fn count_fish_after_days(&mut self, days: usize) -> usize {
+        for _ in 0..days {
+            self.tick();
+        }
+        self.count_fish()
+    }
 }
 
 #[cfg(test)]
@@ -46,14 +77,26 @@ mod tests {
 
     #[test]
     fn part_one_example() {
-        let mut all_fish: Vec<u8> = EXAMPLE_INPUT.clone().into_vec();
-        assert_eq!(count_fish_after_days(&mut all_fish, 18), 26);
-        assert_eq!(count_fish_after_days(&mut all_fish, 80 - 18), 5934);
+        let mut all_fish = FishSimulation::from_fish_list(&EXAMPLE_INPUT).unwrap();
+        assert_eq!(all_fish.count_fish_after_days(18), 26);
+        assert_eq!(all_fish.count_fish_after_days(80 - 18), 5934);
     }
 
     #[test]
     fn part_one_answer() {
         let result = part_one();
-        assert_eq!(result, 351092);
+        assert_eq!(result, Ok(351092));
+    }
+
+    #[test]
+    fn part_two_example() {
+        let mut all_fish = FishSimulation::from_fish_list(&EXAMPLE_INPUT).unwrap();
+        assert_eq!(all_fish.count_fish_after_days(256), 26984457539);
+    }
+
+    #[test]
+    fn part_two_answer() {
+        let result = part_two();
+        assert_eq!(result, Ok(1595330616005));
     }
 }
