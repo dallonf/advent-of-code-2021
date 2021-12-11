1babd9dc26f5cb27e6b7cfe61b5e185ca0f12a33
Dallon Feldner <dallon.feldner@gmail.com>
2021-12-10 19:00:25 -0600

    Day 8: Getting somewhere with this solver

--- src/puzzles/day08/solver.rs
+++ src/puzzles/day08/solver.rs
@@ -1,7 +1,6 @@
-use crate::puzzles::day08::digit::DIGITS;
+use super::digit::{self, DigitDisplay, Segment, SegmentState, ALL_SEGMENTS, DIGITS};
 
-use super::digit::{DigitDisplay, Segment, ALL_SEGMENTS};
-
+#[derive(Debug, PartialEq, Eq)]
 pub struct Solution {
     mapping: [Segment; ALL_SEGMENTS.len()],
 }
@@ -39,6 +38,88 @@ impl Decode for DigitDisplay {
     }
 }
 
+impl Solution {
+    pub fn solve(scrambled_examples: &[DigitDisplay]) -> Result<Self, String> {
+        let mut partial_solution = PartialSolution {
+            possibilities: [digit::ALL_ON; ALL_SEGMENTS.len()],
+        };
+
+        // First: use any simple digits to narrow down the search
+        let simple_digit_examples = scrambled_examples.iter().filter(|&example| {
+            example.is_simple_digit() &&
+            // sorry 8, you're not helpful
+             *example != digit::ALL_ON
+        });
+        for example in simple_digit_examples {
+            let matching_digit = DIGITS
+                .iter()
+                .find(|&it| it.count_segments_on() == example.count_segments_on())
+                .unwrap();
+            let matching_digit_inverted = matching_digit.inverted();
+            // each of the segments in the scrambled example must be a segment in the matching digit...
+            // and each of the segments _off_ must be an off segment!
+            for SegmentState(segment, on) in example.segment_states() {
+                let possibilities_for_segment =
+                    &mut partial_solution.possibilities[segment.as_index()];
+                *possibilities_for_segment = possibilities_for_segment.intersect(if on {
+                    matching_digit
+                } else {
+                    &matching_digit_inverted
+                });
+            }
+        }
+
+        // Next step: Deduction; use any solved segments to solve others
+
+        // Collect possibilities into a single solution, if possible
+        let mapping_results: Box<[Result<Segment, _>]> = partial_solution
+            .possibilities
+            .into_iter()
+            .enumerate()
+            .map(|(i, segment_possibilities)| {
+                let segments_on: Box<[Segment]> = segment_possibilities.segments_on().collect();
+                let segment = Segment::try_from(i as u8).unwrap();
+                match &segments_on[..] {
+                    [single] => Ok(*single),
+                    _ => Err(format!(
+                        "Need exactly one possibility for {:?}: {:?}",
+                        segment, segments_on
+                    )),
+                }
+            })
+            .collect();
+        let errors: Box<[&str]> = mapping_results
+            .iter()
+            .filter_map(|result| match result {
+                Ok(_) => None,
+                Err(err) => Some(err.as_str()),
+            })
+            .collect();
+
+        if errors.len() > 0 {
+            return Err(format!(
+                "Couldn't find a solution: {:?}",
+                partial_solution.possibilities
+            ));
+        }
+
+        let mut mapping = [Segment::A; ALL_SEGMENTS.len()];
+        // since we checked for errors above, we know that we have the right size of results
+        for (i, segment) in mapping_results
+            .into_iter()
+            .map(|it| it.as_ref().unwrap())
+            .enumerate()
+        {
+            mapping[i] = *segment;
+        }
+        Ok(Solution { mapping })
+    }
+}
+
+struct PartialSolution {
+    possibilities: [DigitDisplay; ALL_SEGMENTS.len()],
+}
+
 #[cfg(test)]
 mod tests {
     use std::str::FromStr;
@@ -60,5 +141,31 @@ mod tests {
         };
         let scrambled = DigitDisplay::from_str("cdfbe").unwrap();
         assert_eq!(scrambled.decode_digit(&solution), Ok(5));
+    }
+
+    #[test]
+    fn test_solve() {
+        let examples = [
+            "acedgfb", "cdfbe", "gcdfa", "fbcad", "dab", "cefabd", "cdfgeb", "eafb", "cagedb", "ab",
+        ]
+        .into_iter()
+        .map(|it| DigitDisplay::from_str(it))
+        .collect::<Result<Box<[DigitDisplay]>, _>>()
+        .unwrap();
+        let result = Solution::solve(&examples);
+        assert_eq!(
+            result,
+            Ok(Solution {
+                mapping: [
+                    Segment::C,
+                    Segment::F,
+                    Segment::G,
+                    Segment::A,
+                    Segment::B,
+                    Segment::D,
+                    Segment::E,
+                ],
+            })
+        );
     }
 }
