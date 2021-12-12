063ae162a8477b09bfde38654ae431b939ff5fb0
Dallon Feldner <dallon.feldner@gmail.com>
2021-12-11 19:13:51 -0600

    Day 11 Part One

---
+++ src/puzzles/day11.rs
@@ -0,0 +1,105 @@
+use std::collections::VecDeque;
+
+// Day 11: Dumbo Octopus
+use crate::prelude::*;
+use crate::shared::grid::{Grid, Point};
+
+lazy_static! {
+    static ref PUZZLE_INPUT: OctopusGrid =
+        OctopusGrid::from_digit_lines(&include_lines!("day11_input.txt").collect::<Box<[&str]>>())
+            .unwrap();
+}
+
+pub fn part_one() -> usize {
+    PUZZLE_INPUT.clone().simulate(100)
+}
+
+#[derive(Clone, Debug)]
+struct OctopusGrid(Grid<u8>);
+
+impl OctopusGrid {
+    fn from_digit_lines(lines: &[&str]) -> Result<Self, String> {
+        Ok(OctopusGrid(Grid::from_digit_lines(lines)?))
+    }
+
+    /// Returns the number of flashes
+    fn step(&mut self) -> usize {
+        let mut flashed_this_step: Vec<Point> = Vec::new();
+        let mut increment_queue: VecDeque<Point> = self.0.all_points().collect();
+
+        while let Some(point) = increment_queue.pop_front() {
+            self.0.update(point, |x| *x + 1);
+            if *self.0.get(point) == 10 {
+                flashed_this_step.push(point);
+                for adjacent in self.0.adjacent_points_with_diagonals(point) {
+                    increment_queue.push_back(adjacent);
+                }
+            }
+        }
+
+        for point in flashed_this_step.iter() {
+            self.0.set(*point, 0);
+        }
+
+        flashed_this_step.len()
+    }
+
+    /// Returns the number of flashes
+    fn simulate(&mut self, num_steps: usize) -> usize {
+        (0..num_steps).map(|_| self.step()).sum()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    lazy_static! {
+        static ref EXAMPLE_INPUT: OctopusGrid = OctopusGrid::from_digit_lines(&[
+            "5483143223",
+            "2745854711",
+            "5264556173",
+            "6141336146",
+            "6357385478",
+            "4167524645",
+            "2176841721",
+            "6882881134",
+            "4846848554",
+            "5283751526",
+        ])
+        .unwrap();
+    }
+
+    #[test]
+    fn test_simple_step() {
+        let mut grid =
+            OctopusGrid::from_digit_lines(&["11111", "19991", "19191", "19991", "11111"]).unwrap();
+        assert_eq!(grid.step(), 9);
+    }
+
+    #[test]
+    fn test_steps() {
+        let mut grid = EXAMPLE_INPUT.clone();
+        assert_eq!(grid.step(), 0);
+        assert_eq!(grid.step(), 35);
+    }
+
+    #[test]
+    fn test_quick_example() {
+        let result = EXAMPLE_INPUT.clone().simulate(10);
+        assert_eq!(result, 204);
+    }
+
+    #[test]
+    fn test_long_example() {
+        let result = EXAMPLE_INPUT.clone().simulate(100);
+        assert_eq!(result, 1656);
+    }
+
+    #[test]
+    fn part_one_answer() {
+        let result = part_one();
+        assert_eq!(result, 1697);
+    }
+}
--- src/shared/grid.rs
+++ src/shared/grid.rs
@@ -1,5 +1,7 @@
-use crate::prelude::*;
-use std::fmt::Debug;
+use std::{
+    fmt::{Debug, Display},
+    ops::RangeInclusive,
+};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Point {
@@ -13,7 +15,7 @@ impl Point {
     }
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone)]
 pub struct Grid<T> {
     width: usize,
     data: Box<[T]>,
@@ -28,8 +30,20 @@ impl<T> Grid<T> {
         self.data.len() / self.width
     }
 
+    fn index_of(&self, point: Point) -> usize {
+        point.y * self.width + point.x
+    }
+
     pub fn get(&self, point: Point) -> &T {
-        &self.data[point.y * self.width + point.x]
+        &self.data[self.index_of(point)]
+    }
+
+    pub fn set(&mut self, point: Point, new_val: T) {
+        self.data[self.index_of(point)] = new_val;
+    }
+
+    pub fn update(&mut self, point: Point, new_val_fn: fn(prev: &T) -> T) {
+        self.data[self.index_of(point)] = new_val_fn(&self.data[self.index_of(point)]);
     }
 
     pub fn adjacent_points(&self, point: Point) -> impl Iterator<Item = Point> + '_ {
@@ -44,6 +58,25 @@ impl<T> Grid<T> {
                     None
                 }
             })
+    }
+
+    pub fn adjacent_points_with_diagonals(&self, point: Point) -> impl Iterator<Item = Point> + '_ {
+        let x = point.x as isize;
+        let y = point.y as isize;
+        RangeInclusive::new(x - 1, x + 1).flat_map(move |new_x| {
+            RangeInclusive::new(y - 1, y + 1).filter_map(move |new_y| {
+                if new_x >= 0
+                    && new_x < self.width as isize
+                    && new_y >= 0
+                    && new_y < self.height() as isize
+                    && (new_x, new_y) != (x, y)
+                {
+                    Some(Point::new(new_x as usize, new_y as usize))
+                } else {
+                    None
+                }
+            })
+        })
     }
 
     pub fn all_points(&self) -> impl Iterator<Item = Point> + '_ {
@@ -77,10 +110,39 @@ impl Grid<u8> {
     }
 }
 
+impl<T: Display> Debug for Grid<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let size = format!("{} x {}", self.width(), self.height());
+        let data_strs: Box<[String]> = self.data.iter().map(|data| data.to_string()).collect();
+
+        write!(f, "Grid ({})\n", size)?;
+
+        let max_size = data_strs.iter().map(|it| it.char_indices().count()).max();
+        let data_strs: Box<[String]> = data_strs
+            .into_iter()
+            .map(|it| format!("{:indent$}", it, indent = max_size.unwrap()))
+            .collect();
+
+        let grid_lines: Box<[String]> = (0..self.height())
+            .map(|y| {
+                (0..self.width())
+                    .map(|x| data_strs[self.index_of(Point::new(x, y))].as_str())
+                    .collect::<Box<[&str]>>()
+                    .join("")
+            })
+            .collect();
+
+        let data_str = grid_lines.join("\n");
+
+        write!(f, "{}", data_str)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+    use crate::prelude::*;
+
     lazy_static! {
         static ref EXAMPLE_INPUT: Grid<u8> = Grid::from_digit_lines(&[
             "2199943210",
