d926dafff12d00a1d0c6f8b01c5c7d59bee3e877
Dallon Feldner <dallon.feldner@gmail.com>
2021-12-13 03:16:09 -0600

    Day 13 Part One

--- src/puzzles/day13.rs
+++ src/puzzles/day13.rs
@@ -1,21 +1,159 @@
 // Day 13: Transparent Origami
 use crate::prelude::*;
+use crate::shared::grid::{Grid, Point, SparseGrid};
+use std::str::FromStr;
 
 lazy_static! {
-    static ref PUZZLE_INPUT: Box<[&'static str]> = include_lines!("day00_input.txt").collect();
+    static ref PUZZLE_INPUT: Instructions =
+        Instructions::from_lines(include_lines!("day13_input.txt")).unwrap();
 }
 
-pub fn part_one() -> String {
-    format!("Hello world! ({})", PUZZLE_INPUT.len())
+pub fn part_one() -> usize {
+    PUZZLE_INPUT
+        .grid
+        .fold(PUZZLE_INPUT.fold_instructions[0])
+        .count_dots()
+}
+
+struct OrigamiGrid(SparseGrid<()>);
+
+impl OrigamiGrid {
+    fn fold(&self, instruction: FoldInstruction) -> OrigamiGrid {
+        let max_x = self.0.width() - 1;
+        let max_y = self.0.height() - 1;
+        let mut new_grid = OrigamiGrid(SparseGrid::new());
+        for (point, _) in self.0.all_extant_points() {
+            let Point { x, y } = point;
+            let new_point = match instruction {
+                FoldInstruction::X(fold_x) => {
+                    if x > fold_x {
+                        Point::new(max_x - x, y)
+                    } else {
+                        point
+                    }
+                }
+                FoldInstruction::Y(fold_y) => {
+                    if y > fold_y {
+                        Point::new(x, max_y - y)
+                    } else {
+                        point
+                    }
+                }
+            };
+            new_grid.0.set(new_point, ());
+        }
+        new_grid
+    }
+
+    fn count_dots(&self) -> usize {
+        self.0.all_extant_points().count()
+    }
+}
+
+impl FromIterator<Point> for OrigamiGrid {
+    fn from_iter<T: IntoIterator<Item = Point>>(iter: T) -> Self {
+        let mut grid = OrigamiGrid(SparseGrid::new());
+        for point in iter {
+            grid.0.set(point, ());
+        }
+        grid
+    }
+}
+
+#[derive(PartialEq, Eq, Clone, Copy, Debug)]
+enum FoldInstruction {
+    X(usize),
+    Y(usize),
+}
+
+impl FromStr for FoldInstruction {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (command, number) = s
+            .split_once('=')
+            .ok_or(format!("Invalid fold command: {}", s))?;
+        let number: usize = number.parse().map_err(|_| "Invalid number")?;
+        match command {
+            "fold along x" => Ok(FoldInstruction::X(number)),
+            "fold along y" => Ok(FoldInstruction::Y(number)),
+            _ => Err(format!("Invalid fold command: {}", s)),
+        }
+    }
+}
+
+struct Instructions {
+    grid: OrigamiGrid,
+    fold_instructions: Vec<FoldInstruction>,
+}
+
+impl Instructions {
+    fn from_lines<'a, T: IntoIterator<Item = &'a str>>(iter: T) -> Result<Instructions, String> {
+        let mut iter = iter.into_iter();
+        let points = (&mut iter)
+            .take_while(|it| !it.is_empty())
+            .map(|it| Point::from_str(it))
+            .collect::<Result<Vec<_>, _>>()?;
+        let grid: OrigamiGrid = points.into_iter().collect();
+
+        let fold_instructions = (&mut iter).map(|it| it.parse()).collect::<Result<_, _>>()?;
+
+        Ok(Instructions {
+            grid,
+            fold_instructions,
+        })
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    lazy_static! {
+        static ref EXAMPLE_INPUT: Instructions = Instructions::from_lines(
+            [
+                "6,10",
+                "0,14",
+                "9,10",
+                "0,3",
+                "10,4",
+                "4,11",
+                "6,0",
+                "6,12",
+                "4,1",
+                "0,13",
+                "10,12",
+                "3,4",
+                "3,0",
+                "8,4",
+                "1,10",
+                "2,14",
+                "8,10",
+                "9,0",
+                "",
+                "fold along y=7",
+                "fold along x=5",
+            ]
+            .into_iter()
+        )
+        .unwrap();
+    }
+
+    #[test]
+    fn test_parse() {
+        assert_eq!(EXAMPLE_INPUT.grid.count_dots(), 18);
+        assert_eq!(EXAMPLE_INPUT.fold_instructions.len(), 2);
+    }
+
+    #[test]
+    fn test_fold() {
+        let result = EXAMPLE_INPUT.grid.fold(EXAMPLE_INPUT.fold_instructions[0]);
+        assert_eq!(result.count_dots(), 17);
+    }
+
     #[test]
     fn part_one_answer() {
         let result = part_one();
-        assert_eq!(result, "Hello world! (3)");
+        assert_eq!(result, 710);
     }
 }
