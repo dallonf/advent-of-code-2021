94e862d10c01a928b050f2ed7da1cdcde94fa7b5
Dallon Feldner <dallon.feldner@gmail.com>
2021-12-13 19:23:42 -0600

    Day 10 Part Two

--- src/puzzles/day10.rs
+++ src/puzzles/day10.rs
@@ -8,14 +8,18 @@ lazy_static! {
 }
 
 pub fn part_one() -> Result<u32, String> {
-    compute_score(PUZZLE_INPUT.iter().copied())
+    compute_syntax_score(PUZZLE_INPUT.iter().copied())
+}
+
+pub fn part_two() -> Result<u64, String> {
+    compute_autocomplete_score(PUZZLE_INPUT.iter().copied())
 }
 
 #[derive(Debug, PartialEq, Eq)]
 enum ParseResult {
     Valid,
     UnexpectedToken(char),
-    Incomplete,
+    Incomplete { stack: Vec<char> },
 }
 
 fn expected_closing_token(opening_token: char) -> char {
@@ -44,11 +48,13 @@ fn parse_line(line: &str) -> ParseResult {
     if chunk_stack.is_empty() {
         ParseResult::Valid
     } else {
-        ParseResult::Incomplete
+        ParseResult::Incomplete {
+            stack: chunk_stack.into(),
+        }
     }
 }
 
-fn compute_score<'a, T>(lines: T) -> Result<u32, String>
+fn compute_syntax_score<'a, T>(lines: T) -> Result<u32, String>
 where
     T: IntoIterator<Item = &'a str>,
 {
@@ -66,6 +72,45 @@ where
             _ => Ok(0),
         })
         .try_fold(0, |prev, next| next.map(|next| prev + next))
+}
+
+fn compute_autocomplete_score<'a, T: IntoIterator<Item = &'a str>>(
+    lines: T,
+) -> Result<u64, String> {
+    let scores = lines
+        .into_iter()
+        .map(|line| autocomplete_score_for_result(&parse_line(line)))
+        .collect::<Result<Vec<Option<_>>, _>>()?;
+    let scores = scores
+        .into_iter()
+        .filter_map(|it| it)
+        .sorted()
+        .collect_vec();
+
+    if scores.len() == 0 {
+        return Err("No lines autocompleted".to_string());
+    }
+
+    Ok(scores[scores.len() / 2])
+}
+
+fn autocomplete_score_for_result(parse_result: &ParseResult) -> Result<Option<u64>, String> {
+    if let ParseResult::Incomplete { stack } = parse_result {
+        stack
+            .iter()
+            .rev()
+            .map(|c| match c {
+                '(' => Ok(1),
+                '[' => Ok(2),
+                '{' => Ok(3),
+                '<' => Ok(4),
+                &other_char => Err(format!("invalid token: {}", other_char)),
+            })
+            .try_fold(0 as u64, |prev, next| Ok(prev * 5 + next?))
+            .map(|it| Some(it))
+    } else {
+        Ok(None)
+    }
 }
 
 #[cfg(test)]
@@ -97,13 +142,15 @@ mod tests {
     fn test_incomplete_line() {
         assert_eq!(
             parse_line("(((({<>}<{<{<>}{[]{[]{}"),
-            ParseResult::Incomplete
+            ParseResult::Incomplete {
+                stack: "((((<{<{{".chars().collect()
+            }
         );
     }
 
     #[test]
-    fn test_score() {
-        let result = compute_score(EXAMPLE_INPUT.iter().copied());
+    fn test_syntax_score() {
+        let result = compute_syntax_score(EXAMPLE_INPUT.iter().copied());
         assert_eq!(result, Ok(26397));
     }
 
@@ -111,5 +158,25 @@ mod tests {
     fn part_one_answer() {
         let result = part_one();
         assert_eq!(result, Ok(367059));
+    }
+
+    #[test]
+    fn test_autocomplete_score() {
+        let result = autocomplete_score_for_result(&parse_line(EXAMPLE_INPUT[0]));
+        assert_eq!(result, Ok(Some(288957)));
+        let result = autocomplete_score_for_result(&parse_line(EXAMPLE_INPUT[1]));
+        assert_eq!(result, Ok(Some(5566)));
+    }
+
+    #[test]
+    fn test_autocomplete_aggregation() {
+        let result = compute_autocomplete_score(EXAMPLE_INPUT.iter().copied());
+        assert_eq!(result, Ok(288957));
+    }
+
+    #[test]
+    fn part_two_answer() {
+        let result = part_two();
+        assert_eq!(result, Ok(1952146692));
     }
 }
