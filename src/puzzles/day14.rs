4be988f0136badbc03e727618ed7efdefdf4a844
Dallon Feldner <dallon.feldner@gmail.com>
2021-12-14 19:48:39 -0600

    Day 14 Part Two

--- src/puzzles/day14.rs
+++ src/puzzles/day14.rs
@@ -1,4 +1,7 @@
-use std::{borrow::Cow, collections::HashMap};
+use std::{
+    borrow::Cow,
+    collections::{hash_map::Entry, HashMap, HashSet},
+};
 
 // Day 0: Template
 use crate::prelude::*;
@@ -7,14 +10,22 @@ lazy_static! {
     static ref PUZZLE_INPUT: Input = Input::from_lines(include_lines!("day14_input.txt")).unwrap();
 }
 
-pub fn part_one() -> usize {
+pub fn part_one() -> u64 {
     let polymer = PUZZLE_INPUT.steps(10);
     frequency_score(&polymer)
 }
 
+pub fn part_two() -> u64 {
+    let frequency_map = PUZZLE_INPUT.simulate_steps(40);
+    frequency_score_from_map(&frequency_map)
+}
+
+type CharPair = (char, char);
+type FrequencyMap = HashMap<char, u64>;
+
 struct Input {
     polymer: Vec<char>,
-    insertion_rules: HashMap<(char, char), char>,
+    insertion_rules: HashMap<CharPair, char>,
 }
 
 impl Input {
@@ -61,10 +72,95 @@ impl Input {
             })
             .into_owned()
     }
+
+    fn simulate_steps(&self, count: usize) -> FrequencyMap {
+        let mut memo = MemoizedPolymerExpander::new(self);
+        let starting_map: FrequencyMap = self
+            .polymer
+            .iter()
+            .copied()
+            .counts()
+            .into_iter()
+            .map(|(char, count)| (char, count as u64))
+            .collect();
+
+        self.polymer
+            .windows(2)
+            .map(|window| -> FrequencyMap { memo.expand_polymer((window[0], window[1]), count) })
+            .fold(starting_map, |prev, next| add_frequency_maps(&prev, &next))
+    }
 }
 
-fn frequency_score(input: &[char]) -> usize {
-    let frequency_map = input.iter().copied().counts();
+struct MemoizedPolymerExpander<'a> {
+    memos: HashMap<(CharPair, usize), FrequencyMap>,
+    input: &'a Input,
+}
+
+impl<'a> MemoizedPolymerExpander<'a> {
+    fn new(input: &'a Input) -> Self {
+        MemoizedPolymerExpander {
+            memos: HashMap::new(),
+            input,
+        }
+    }
+
+    fn expand_polymer(&mut self, pair: CharPair, remaining_count: usize) -> FrequencyMap {
+        if remaining_count == 0 {
+            return FrequencyMap::new();
+        }
+
+        let key = (pair, remaining_count);
+        if let Some(memoized_result) = self.memos.get(&key) {
+            return memoized_result.to_owned();
+        }
+
+        if let Some(&inserted_char) = self.input.insertion_rules.get(&pair) {
+            let a = self.expand_polymer((pair.0, inserted_char), remaining_count - 1);
+            let b = self.expand_polymer((inserted_char, pair.1), remaining_count - 1);
+
+            let mut result = add_frequency_maps(&a, &b);
+            match result.entry(inserted_char) {
+                Entry::Occupied(mut entry) => {
+                    *entry.get_mut() += 1;
+                }
+                Entry::Vacant(entry) => {
+                    entry.insert(1);
+                }
+            }
+
+            self.memos.insert(key, result.clone());
+            result
+        } else {
+            FrequencyMap::new()
+        }
+    }
+}
+
+fn add_frequency_maps(a: &FrequencyMap, b: &FrequencyMap) -> FrequencyMap {
+    let all_keys: HashSet<char> = a.keys().chain(b.keys()).copied().collect();
+    all_keys
+        .into_iter()
+        .map(|char| {
+            (
+                char,
+                a.get(&char).unwrap_or(&0) + b.get(&char).unwrap_or(&0),
+            )
+        })
+        .collect()
+}
+
+fn frequency_score(input: &[char]) -> u64 {
+    let frequency_map = input
+        .iter()
+        .copied()
+        .counts()
+        .into_iter()
+        .map(|(char, count)| (char, count as u64))
+        .collect();
+    frequency_score_from_map(&frequency_map)
+}
+
+fn frequency_score_from_map(frequency_map: &FrequencyMap) -> u64 {
     let most_frequent = frequency_map
         .iter()
         .map(|(_, count)| *count)
@@ -123,5 +219,18 @@ mod tests {
     fn part_one_answer() {
         let result = part_one();
         assert_eq!(result, 2851);
+    }
+
+    #[test]
+    fn test_bigger() {
+        let frequency_map = EXAMPLE_INPUT.simulate_steps(40);
+        let result = frequency_score_from_map(&frequency_map);
+        assert_eq!(result, 2188189693529);
+    }
+
+    #[test]
+    fn part_two_answer() {
+        let result = part_two();
+        assert_eq!(result, 10002813279337);
     }
 }
